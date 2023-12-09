5d667fc6ac58e021f8aef1d310089d21cccca5ea
Javohir <narzullayevj99@gmail.com>
2023-12-10 01:20:57 +0500

    calculation ready

--- src/main/java/uz/tenge/totalscore/totalscore/TotalScoreApplication.java
+++ src/main/java/uz/tenge/totalscore/totalscore/TotalScoreApplication.java
@@ -8,6 +8,9 @@ public class TotalScoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TotalScoreApplication.class, args);
+
+        //TODO test calculation and another case again
+        //TODO write documentation to start proejct
     }
 
 }
